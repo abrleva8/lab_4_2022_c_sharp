f0b7fd2824e2e14e71ede45fc131bcce27b5d71f
abrleva8@gmail.com <pT6n6TFrSFjcJAu>
2022-04-24 14:43:21 +0300

    Add project files.

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace lab_4 {
+    partial class MainWindow {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.dataGridView_dataBase = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_dataBase)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_dataBase
+            // 
+            this.dataGridView_dataBase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_dataBase.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView_dataBase.Name = "dataGridView_dataBase";
+            this.dataGridView_dataBase.Size = new System.Drawing.Size(781, 376);
+            this.dataGridView_dataBase.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView_dataBase);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "DataBaseApplication";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_dataBase)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_dataBase;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace lab_4 {
+    public partial class MainWindow : Form {
+        public MainWindow() {
+            InitializeComponent();
+            InitializeConnection();
+        }
+
+        private void InitializeConnection() {
+            SQLiteConnection sqLiteConnection = new SQLiteConnection("data source=bank.db");
+
+            string query = "SELECT* from accounts";
+            SQLiteCommand cmd = new SQLiteCommand(query, sqLiteConnection);
+
+            DataTable dataTable = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            adapter.Fill(dataTable);
+
+            dataGridView_dataBase.DataSource = dataTable;
+        }
+    }
+}
